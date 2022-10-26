7aa4f0348e445dffe407281813e281dc061003c9
inrongpon22 <inrongpon.phanupong2@gmail.com>
2022-10-26 20:41:53 +0700

    edit tailwindconfig

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,8 +7,8 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'intro-desktop': "url('./src/assets/images/bg-curvy-desktop.svg')",
-        'intro-mobile': "url('./src/assets/images/bg-curvy-mobile.svg')",
+        'intro-desktop': "url('/src/assets/images/bg-curvy-desktop.svg')",
+        'intro-mobile': "url('/src/assets/images/bg-curvy-mobile.svg')",
       },
       fontFamily: {
         'raleway': ['Raleway', 'sans-serif'],
